de2661d6dc5ecc9ced4004f716afe121c43a1a3f
leonardo-weber <weberleonardo2002@gmail.com>
2024-06-21 18:05:03 -0300

    couple more

--- blog/views.py
+++ blog/views.py
@@ -1,146 +1,17 @@
-from django.shortcuts import render
-from datetime import date
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Léo",
-        "date": date(2024, 6, 18),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains",
-        "content": """
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Léo",
-        "date": date(2024, 6, 18),
-        "title": "into the woods",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains",
-        "content": """
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-        """
-    },
-    {
-        "slug": "coding",
-        "image": "coding.jpg",
-        "author": "Léo",
-        "date": date(2024, 6, 18),
-        "title": "coding",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains",
-        "content": """
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-        """
-    },
-       {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Léo",
-        "date": date(2024, 6, 18),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains",
-        "content": """
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Léo",
-        "date": date(2024, 6, 18),
-        "title": "into the woods",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains",
-        "content": """
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-        """
-    },
-    {
-        "slug": "coding",
-        "image": "coding.jpg",
-        "author": "Léo",
-        "date": date(2024, 6, 18),
-        "title": "coding",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains",
-        "content": """
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Repudiandae perferendis consequatur sequi dignissimos a ullam voluptatum veniam doloribus! 
-            Quibusdam quas vitae soluta, molestias doloribus perspiciatis assumenda eligendi rerum expedita minus!
-        """
-    }
-]
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def get_date(post):
-    return post.get('date')
+    return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", { "posts": latest_posts })
+    posts = Post.objects.all().order_by('-date')[:3]
+    return render(request, "blog/index.html", { "posts": posts })
 
 def posts (request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/posts.html', { "all_posts": all_posts })
 
 def post_detail(request, slug):
-    post = next(post for post in all_posts if post['slug'] == slug)
+    post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', { "post": post })
